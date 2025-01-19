0824f9188b38517dd1893550245b12f9147fc54e
Won Lee <wonseob2207@gmail.com>
2025-01-19 16:19:32 -0600

    init for php

--- for.php
+++ for.php
@@ -1,3 +1,13 @@
 <?php
-echo "Rocket launch countdown:\n"
+
+echo "Rocket launch countdown:\n";
+
+for ($i = 10; $i > 0; $i--) {
+    echo $i . "...";
+    if (1 === $i) {
+        echo "Liftoff! \n";
+    }
+    sleep(1);
+}
+
 ?>
