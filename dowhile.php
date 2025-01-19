d9251ee7ce25dffcbd3e83a890d1478afc70480d
Won Lee <wonseob2207@gmail.com>
2025-01-19 16:24:06 -0600

    init dowhile php

---
+++ dowhile.php
@@ -0,0 +1,10 @@
+<?php
+do{
+    $diceRoll = rand(1, 6);
+    echo "You rolled a $diceRoll\n";
+    if (6 == $diceRoll){
+        echo "Congrats! You hit the jackpot! \n";
+    }
+    // loop body
+} while ();
+?>
