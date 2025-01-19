1df8c12596cab11afd2f619f350503197ecc328e
Won Lee <wonseob2207@gmail.com>
2025-01-19 16:40:33 -0600

    edited while php

--- while.php
+++ while.php
@@ -1,16 +1,19 @@
 <?php
 
+// Declaring variables
 $secret = "magic";
 $attempts = 0;
 $maxAttempts = 5;
-
+// While loop 
 while ($attempts < $maxAttempts){
+
     echo "Guess the password ";
     $guess = trim(fgets(STDIN));
     $attempts++;
-
+    // If loop
     if ($guess == $secret) {
         echo "Password Accepted, Access Granted\n";
+        // Break loop once password is guessed correctly
         break;
     } elseif ($attempts == $maxAttempts) {
         echo "Too many attempts, Access denied.\n";
