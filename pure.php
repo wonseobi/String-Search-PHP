ff481f3ef7a4858e5a3b40d560b2fbd0e38bb546
Won Lee <wonseob2207@gmail.com>
2025-02-05 12:42:34 -0600

    finished pure php

---
+++ pure.php
@@ -0,0 +1,20 @@
+<?php
+
+function add(int $a, int $b): int{
+    return $a + $b;
+}
+
+var_dump(add (1,3), add(1,3));
+
+// defining global value for addToTotal function
+$total = 0;
+
+function addToTotal($value){
+    global $total; 
+    $total += $value;
+    return $total;
+}
+
+var_dump(addToTotal(3), addToTotal(3));
+
+?>
