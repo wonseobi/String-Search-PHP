b0f2cce0051ffc337e54d78f5e01a76be29342a4
Won Lee <wonseob2207@gmail.com>
2025-02-06 10:26:11 -0600

    init for strings php

---
+++ strings.php
@@ -0,0 +1,20 @@
+<?php
+
+$name = "Won";
+echo "Hello, $name! \n";
+echo 'Hello, $name! \n';
+
+$heredoc = <<<EOD
+Multi-line string
+with variable $name
+EOD;
+
+$nowdoc = <<<'EOD'
+Multi-line string
+with variable $name
+EOD;
+
+echo $heredoc;
+echo $nowdoc;
+
+?>
