0877f065473dfb1c9e92ce3f53c0bc0e2bcd3963
Won Lee <wonseob2207@gmail.com>
2025-02-06 17:24:10 -0600

    second commit string-search.php

--- string-search.php
+++ string-search.php
@@ -1,5 +1,15 @@
 <?php
 
+$haystack = "The quick brown fox";
+// Return string index position (strpos)
+$pos = strpos($haystack, "quick");
+var_dump($pos);
 
+// Replace a substring with another substring (str_replace)
+var_dump(str_replace("quick", "lazy", $haystack));
+
+// Find words with {5} characters with (preg_match_all) in $haystack and then store them in $matches variable.
+preg_match_all('/\w{5}/', $haystack, $matches);
+var_dump($matches);
 
 ?>
